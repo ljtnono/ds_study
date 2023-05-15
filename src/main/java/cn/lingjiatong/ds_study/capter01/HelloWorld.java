a630bcae2af5f439f574eb085be9dbda14a7d5ad
lingjiatong <935188400@qq.com>
2023-05-15 17:10:34 +0800

    [feat]初始化项目

---
+++ src/main/java/cn/lingjiatong/ds_study/capter01/HelloWorld.java
@@ -0,0 +1,14 @@
+package cn.lingjiatong.ds_study.capter01;
+
+/**
+ * hello world
+ *
+ * @author Ling, Jiatong
+ * Date: 2023/5/15 17:09
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
